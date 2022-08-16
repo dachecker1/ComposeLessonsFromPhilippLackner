b8108dd1b28624d52dfa0c7c661177f46d1539a7
andrenivisnk <andrenivinsk@gmail.com>
2022-08-16 11:11:52 +0300

    part 6 done saving state for views

--- app/src/main/java/com/vk/composelessonsfromphilipplackner/MainActivity.kt
+++ app/src/main/java/com/vk/composelessonsfromphilipplackner/MainActivity.kt
@@ -1,78 +1,61 @@
 package com.vk.composelessonsfromphilipplackner
 
 import android.os.Bundle
-import android.text.style.StrikethroughSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fontFamily = FontFamily(
-            Font(R.font.dynapuff_medium, FontWeight.Medium),
-            Font(R.font.dynapuff_semibold, FontWeight.SemiBold),
-            Font(R.font.dynapuff_bold, FontWeight.Bold),
-            Font(R.font.dynapuff_regular, FontWeight.Normal)
-        )
-
         setContent {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFF101010))
-                .padding(10.dp)
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        ) {
-                            append("J")
-                        }
-                        append("etpack")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        ) {
-                            append("C")
-                        }
-                        append("ompose")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Medium,
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center,
-                    textDecoration = TextDecoration.Underline
-                )
+            Column(modifier = Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+                Box(modifier = Modifier
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize())
             }
         }
     }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit,
+) {
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1F
+                )
+            )
+        }
+    )
 }
 
 
