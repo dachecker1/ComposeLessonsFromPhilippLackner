8084eed5958b9b500da48790ac27bdcafc80b609
andrenivisnk <andrenivinsk@gmail.com>
2022-08-16 10:45:50 +0300

    part 5 done add different fonts use different style for one string

--- app/src/main/java/com/vk/composelessonsfromphilipplackner/MainActivity.kt
+++ app/src/main/java/com/vk/composelessonsfromphilipplackner/MainActivity.kt
@@ -1,86 +1,78 @@
 package com.vk.composelessonsfromphilipplackner
 
 import android.os.Bundle
+import android.text.style.StrikethroughSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.dynapuff_medium, FontWeight.Medium),
+            Font(R.font.dynapuff_semibold, FontWeight.SemiBold),
+            Font(R.font.dynapuff_bold, FontWeight.Bold),
+            Font(R.font.dynapuff_regular, FontWeight.Normal)
+        )
+
         setContent {
-
-            val painter = painterResource(id = R.drawable.fito)
-            val desc = "Fito girl"
-            val title = "this is a girl"
-            Box(modifier = Modifier.fillMaxWidth(0.5f).padding(10.dp)) {
-                ImageCard(painter = painter, contentDescription = desc, title = title)
-            }
-
-
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier,
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(12.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
             Box(modifier = Modifier
                 .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 300f
-                    )
-                )
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
+                .background(Color(0xFF101010))
+                .padding(10.dp)
             ) {
                 Text(
-                    text = title,
-                    style = TextStyle(color = Color.White, fontSize = 16.sp)
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Medium,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
                 )
             }
         }
     }
 }
+
 
